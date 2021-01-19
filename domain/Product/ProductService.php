e5b383f1143dc0000e2a2f84ee05866a33dc658b
Your Name <shameeindrajith1995@gmail.com>
2021-01-19 22:10:33 +0530

    updated

--- domain/Product/ProductService.php
+++ domain/Product/ProductService.php
@@ -31,10 +31,28 @@ class ProductService{
     }
 
 //find product data
-   public function getProductData($id)
-   {
-    return $this->product->find($id);
-   }
+    public function getProductData($id)
+    {
+     return $this->product->find($id);
+    }
+
+//update category
+    public function updateProduct(Product $product, $request)
+    {
+     $image=$request->file('image');
+     $image_name=time().'.'.$image->extension();
+     $image->move(public_path('images'),$image_name);
+     $data=$request->all();
+     $data['image']=$image_name;
+     return $product->update($this->edit($product, $data));
+    }
+
+    protected function edit(Product $product, $data)
+    {
+     return array_merge($product->toArray(), $data);
+    }
 }
 
 ?>
+
+
