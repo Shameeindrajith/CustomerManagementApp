67ec3a7000b9498383ba48cf41a31af4836be334
Your Name <shameeindrajith1995@gmail.com>
2021-01-19 13:00:06 +0530

    updated

--- domain/Product/ProductService.php
+++ domain/Product/ProductService.php
@@ -5,42 +5,36 @@ use App\Models\Category;
 use App\Models\Product;
 
 class ProductService{
-    
+
     protected $product;
-    
+
     public function __construct()
     {
-        $this->product=new Product();
+     $this->product=new Product();
     }
 
 //get all product data
     public function getAllData()
     {
-        $data=Product::all();
-        return $data;
+     return $this->product->get();
     }
 
-//store product data 
+//store product data
     public function store($request)
     {
-       
-     $this->product->cat_id=$request->category;
-     $this->product->pro_name=$request->pro_name;
-     $this->product->pro_price=$request->pro_price;
-
-     $image=$request->file('file');
+     $image=$request->file('image');
      $image_name=time().'.'.$image->extension();
      $image->move(public_path('images'),$image_name);
-     $this->product->image=$image_name;
-     $this->product->save();
+     $data=$request->all();
+     $data['image']=$image_name;
+     return $this->product->create($data);
     }
 
 //find product data
    public function getProductData($id)
    {
-      $data=Product::find($id);
-      return $data;
-   }   
+    return $this->product->find($id);
+   }
 }
 
 ?>
