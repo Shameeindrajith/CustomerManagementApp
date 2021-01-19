005d421a6a5d2e809d45027feaecfa2d3f31d3f8
Your Name <shameeindrajith1995@gmail.com>
2021-01-19 12:59:15 +0530

    updated

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -12,46 +12,44 @@ class ProductController extends ParentController
 {
 
 //* open product page
-public function index()
-{
-    $response['categaries']=CategoryFacade::getAllData();
-    $response['products']=ProductFacade::getAllData();
-    return view('product')->with($response);
-}
+  public function index()
+  {
+   $response['categaries']=CategoryFacade::getAllData();
+   $response['products']=ProductFacade::getAllData();
+   return view('product')->with($response);
+  }
 
 //* product save function
-public function proStore(Request $request)
-{
-    ProductFacade::store($request);
-    return redirect()->back();
-}
+  public function proStore(Request $request)
+  {
+   ProductFacade::store($request);
+   return redirect()->back();
+  }
 
 //* product delete function
-public function deleteProduct($id)
-{
-    $delete=ProductFacade::getProductData($id);
-    $delete->delete();
-    return redirect()->back();
-}
+  public function deleteProduct($id)
+  {
+   $delete=ProductFacade::getProductData($id);
+   $delete->delete();
+   return redirect()->back();
+  }
 
 //* product update function start
-public function updateProduct($id)
-{   $update=ProductFacade::getProductData($id);
-    $response['categaries']=CategoryFacade::getAllData();
-    $response['products']=ProductFacade::getProductData($id);
-    return view('productUpdate')->with($response);
-
-}
+  public function updateProduct($id)
+  {
+   $response['categaries']=CategoryFacade::getAllData();
+   $response['products']=ProductFacade::getProductData($id);
+   return view('productUpdate')->with($response);
+  }
+  
 // update second function start
-public function update(Request $update)
-{
-    $id=$update->id;
-    $product_data=ProductFacade::getProductData($id);
-    ProductFacade::store($product_data);
-    
-    return redirect(route('product-all'));
+  public function update(Request $update)
+  {
+   $id=$update->id;
+   $product_data=ProductFacade::getProductData($id);
+   ProductFacade::store($product_data);
+   return redirect(route('product-all'));
+  }
 
-}
-//* product update function closed
 
 }
