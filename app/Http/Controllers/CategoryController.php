c217eb525080572fe39c1ca031d95fd517a5b1a1
Your Name <shameeindrajith1995@gmail.com>
2021-01-19 12:59:05 +0530

    updated

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,42 +9,41 @@ use App\Models\Category;
 class CategoryController extends ParentController
 {
 //* open category page
-    public function index() {
-
-        $catData=CategoryFacade::getAllData();
-        return view('category')->with('categories',$catData);
+    public function index()
+    {
+     $catData=CategoryFacade::getAllData();
+     return view('category')->with('categories',$catData);
     }
 
 //* Category store part
     public function catStore(Request $request)
-    {  
-        CategoryFacade::store($request); //calling to categoryfacade 
-       return redirect()->back();
+    {
+     CategoryFacade::store($request); //calling to categoryfacade
+     return redirect()->back();
     }
 //* Category delete part
     public function deleteCategory($id)
-    {   
-        $delete=CategoryFacade::getCategoryData($id);
-        $delete->delete();
-        return redirect()->back();
+    {
+     $delete=CategoryFacade::getCategoryData($id);
+     $delete->delete();
+     return redirect()->back();
     }
 
 //* Category update function
     public function updateCategory($id)
     {
-        $update=CategoryFacade::getCategoryData($id);
-        return view('categoryUpdate')->with('update_categories_data',$update);
+     $update=CategoryFacade::getCategoryData($id);
+     return view('categoryUpdate')->with('update_categories_data',$update);
     }
 
 // update second function start
     public function update(Request $updatedata)
     {
-        $id=$updatedata->id;
-        $name=$updatedata->name;
-        $updatecatdata=CategoryFacade::getCategoryData($id);
-        CategoryFacade::store($updatedata);
-        return redirect(route('category-all'));
-
+     $id=$updatedata->id;
+     $name=$updatedata->name;
+     $updatecatdata=CategoryFacade::getCategoryData($id);
+     CategoryFacade::store($updatedata);
+     return redirect(route('category-all'));
     }
-//* Category update function closed
+
 }
